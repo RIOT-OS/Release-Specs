e5f792ffab8cae5fd04902c25eea46c9e311d0ee
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-11-07 16:11:47 +0100

    03: add __main__ guard

--- 03-single-hop-ipv6-icmp/task01.py
+++ 03-single-hop-ipv6-icmp/task01.py
@@ -11,44 +11,45 @@ PAYLOAD_SIZE = 0
 ERROR_TOLERANCE = 1  # %
 DELAY = 10  # ms
 
-p = argparse.ArgumentParser()
-p.add_argument('riotbase', nargs='?',
-               help='Location of RIOT folder')
-args = p.parse_args()
-riotbase = args.riotbase
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument('riotbase', nargs='?',
+                   help='Location of RIOT folder')
+    args = p.parse_args()
+    riotbase = args.riotbase
 
-if not riotbase:
-    p.print_help()
-    sys.exit(1)
+    if not riotbase:
+        p.print_help()
+        sys.exit(1)
 
-os.chdir(os.path.join(riotbase, "tests/gnrc_udp"))
+    os.chdir(os.path.join(riotbase, "tests/gnrc_udp"))
 
-bootstrap("native")
-N = 1
-results = []
+    bootstrap("native")
+    N = 1
+    results = []
 
-try:
-    native_cmd = "make PORT={} BOARD=native term"
-    source = SingleHopNode(native_cmd.format("tap0"))
-    dest = SingleHopNode(native_cmd.format("tap1"))
-    for i in range(N):
-        ip_dest = "ff02::1/64"
-        count = 1000
+    try:
+        native_cmd = "make PORT={} BOARD=native term"
+        source = SingleHopNode(native_cmd.format("tap0"))
+        dest = SingleHopNode(native_cmd.format("tap1"))
+        for i in range(N):
+            ip_dest = "ff02::1/64"
+            count = 1000
 
-        packet_loss = ping(source, dest, ip_dest, count, PAYLOAD_SIZE, DELAY)
-        results.append(packet_loss)
+            packet_loss = ping(source, dest, ip_dest, count, PAYLOAD_SIZE, DELAY)
+            results.append(packet_loss)
 
-        assert(packet_loss < ERROR_TOLERANCE * count)
-        print("OK")
+            assert(packet_loss < ERROR_TOLERANCE * count)
+            print("OK")
 
-except Exception as e:
-    print(str(e))
-    print("Test failed!")
-    source.stop()
-    dest.stop()
-    sys.exit(1)
-finally:
-    source.stop()
-    dest.stop()
+    except Exception as e:
+        print(str(e))
+        print("Test failed!")
+        source.stop()
+        dest.stop()
+        sys.exit(1)
+    finally:
+        source.stop()
+        dest.stop()
 
-print_results(results)
+    print_results(results)
