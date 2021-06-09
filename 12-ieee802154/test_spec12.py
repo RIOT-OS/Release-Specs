4f5a8a08adfb32aac959b6c1e7cc765fed034ea0
Lars <lars.kowoll@haw-hamburg.de>
2021-06-09 13:04:30 +0200

    12-ieee802154/test_spec12.py changed function names

--- 12-ieee802154/test_spec12.py
+++ 12-ieee802154/test_spec12.py
@@ -1,7 +1,7 @@
 from time import sleep
 import pytest
 
-from riotctrl_shell.ieee802154 import Config_phy, Print_addr, Txtsnd
+from riotctrl_shell.ieee802154 import IEEE802154Phy
 
 APP = 'tests/ieee802154_hal'
 PHY_MODE = 'O-QPSK'
@@ -9,7 +9,7 @@ DEFAULT_CHANNEL = 26
 pytestmark = pytest.mark.rc_only()
 
 
-class Shell(Config_phy, Print_addr, Txtsnd):
+class Shell(IEEE802154Phy):
     pass
 
 
@@ -21,17 +21,17 @@ def test_task01(riot_ctrl):
         riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0'),
         riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1'),
     )
-    sender.tx_mode("direct")
-    receiver.tx_mode("direct")
-    receiver_addr = receiver.print_addr()
-    sender_addr = sender.print_addr()
+    sender.ieee802154_tx_mode("direct")
+    receiver.ieee802154_tx_mode("direct")
+    receiver_addr = receiver.ieee802154_print_addr()
+    sender_addr = sender.ieee802154_print_addr()
     for i in range(11, 27):
-        sender.config_phy(PHY_MODE, i, 0)
-        receiver.config_phy(PHY_MODE, i, 0)
-        sleep(0.5)
-        sender.txtsnd(receiver_addr, 1, 0)
-        sleep(0.5)
-        receiver.check_last_packet(sender_addr, i)
+        sender.ieee802154_config_phy(PHY_MODE, i, 0)
+        receiver.ieee802154_config_phy(PHY_MODE, i, 0)
+        sleep(1)
+        sender.ieee802154_txtspam(receiver_addr, 1, 0)
+        sleep(1)
+        receiver.ieee802154_check_last_packet(sender_addr, i)
         sleep(0.5)
 
 
@@ -43,10 +43,10 @@ def test_task02(riot_ctrl):
         riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0'),
         riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1'),
     )
-    sender.tx_mode("direct")
-    receiver.tx_mode("direct")
-    receiver_addr = receiver.print_addr()
-    sender.txtsnd(receiver_addr, 5, 1000)
+    sender.ieee802154_tx_mode("direct")
+    receiver.ieee802154_tx_mode("direct")
+    receiver_addr = receiver.ieee802154_print_addr()
+    sender.ieee802154_txtspam(receiver_addr, 5, 1000)
 
 
 @pytest.mark.parametrize('nodes',
@@ -57,10 +57,10 @@ def test_task03(riot_ctrl):
         riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0'),
         riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1'),
     )
-    sender.tx_mode("direct")
-    receiver.tx_mode("direct")
-    receiver_addr = receiver.print_addr()
-    receiver.reply()
-    sender.txtsnd(receiver_addr, 1, 0)
+    sender.ieee802154_tx_mode("direct")
+    receiver.ieee802154_tx_mode("direct")
+    receiver_addr = receiver.ieee802154_print_addr()
+    receiver.ieee802154_reply()
+    sender.ieee802154_txtsnd(receiver_addr, 1)
     sleep(1)
-    sender.check_last_packet(receiver_addr, DEFAULT_CHANNEL)
+    sender.ieee802154_check_last_packet(receiver_addr, DEFAULT_CHANNEL)
