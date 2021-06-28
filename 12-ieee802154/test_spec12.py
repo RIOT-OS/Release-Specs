cb0d3c6a55b1ce0137d1b5fc2488667d5a6fc76d
Lars <lars.kowoll@haw-hamburg.de>
2021-06-28 12:18:46 +0200

    12-ieee802154/test_spec12.py refactor function names

--- 12-ieee802154/test_spec12.py
+++ 12-ieee802154/test_spec12.py
@@ -19,21 +19,21 @@ class Shell(IEEE802154Phy):
                          indirect=['nodes'])
 def test_task01(riot_ctrl):
     sender, receiver = (
-        riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0'),
-        riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1'),
+        riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0', cflags="-DRIOTCTRL"),
+        riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1', cflags="-DRIOTCTRL"),
     )
     sender.ieee802154_tx_mode("direct")
     receiver.ieee802154_tx_mode("direct")
-    receiver_addr = receiver.ieee802154_get_phy()
-    sender_addr = sender.ieee802154_get_phy()
+    receiver_addr = receiver.ieee802154_print_info()[0]
+    sender_addr = sender.ieee802154_print_info()[0]
     for i in range(11, 27):
         sender.ieee802154_config_phy(PHY_MODE, i, 0)
         receiver.ieee802154_config_phy(PHY_MODE, i, 0)
-        sleep(1)
-        sender.ieee802154_txtspam(receiver_addr, 10, 1, 0, 0)
-        sleep(1)
+        sleep(2)
+        sender.ieee802154_txtspam(receiver_addr, 10, 4, 100)
+        sleep(2)
         receiver.ieee802154_check_last_packet(sender_addr, i)
-        sleep(0.5)
+        sleep(1)
 
 
 @pytest.mark.local_only
@@ -42,13 +42,16 @@ def test_task01(riot_ctrl):
                          indirect=['nodes'])
 def test_task02(riot_ctrl):
     sender, receiver = (
-        riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0'),
-        riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1'),
+        riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0', cflags="-DRIOTCTRL"),
+        riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1', cflags="-DRIOTCTRL"),
     )
     sender.ieee802154_tx_mode("direct")
     receiver.ieee802154_tx_mode("direct")
-    receiver_addr = receiver.ieee802154_get_phy()
-    sender.ieee802154_txtspam(receiver_addr, 10, 5, 1000, 0)
+    receiver_addr = receiver.ieee802154_print_info()[0]
+    res = sender.ieee802154_txtspam(receiver_addr, 10, 20, 1000)
+    assert res["tx_packets"] == 20
+    if res["percentage_ack"]:
+        assert res["percentage_ack"] >= 90
 
 
 @pytest.mark.local_only
@@ -57,13 +60,17 @@ def test_task02(riot_ctrl):
                          indirect=['nodes'])
 def test_task03(riot_ctrl):
     sender, receiver = (
-        riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0'),
-        riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1'),
+        riot_ctrl(0, APP, Shell, port='/dev/ttyUSB0', cflags="-DRIOTCTRL"),
+        riot_ctrl(1, APP, Shell, port='/dev/ttyUSB1', cflags="-DRIOTCTRL"),
     )
     sender.ieee802154_tx_mode("direct")
     receiver.ieee802154_tx_mode("direct")
-    receiver_addr = receiver.ieee802154_get_phy()
-    receiver.ieee802154_reply(1)
-    sender.ieee802154_txtsnd(receiver_addr, 10, 0)
+    receiver_addr = receiver.ieee802154_print_info()[0]
+    receiver.ieee802154_reply_mode_cmd("on")
+    res = sender.ieee802154_txtspam(receiver_addr, 10, 10, 100)
     sleep(1)
     sender.ieee802154_check_last_packet(receiver_addr, DEFAULT_CHANNEL)
+    assert res["tx_packets"] == 10
+    if res["percentage_ack"]:
+        assert res["percentage_ack"] >= 90
+    assert res["percentage_rx"] >= 90
