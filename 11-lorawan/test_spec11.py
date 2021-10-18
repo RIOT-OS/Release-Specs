3777026da4d0ceee404c338d2ed093e858fa7892
Francisco Molina <femolina@uc.cl>
2021-10-18 15:09:29 +0200

    11-lorawan: add tasks 01-04

--- 11-lorawan/test_spec11.py
+++ 11-lorawan/test_spec11.py
@@ -1,12 +1,17 @@
 import time
 import pytest
+from riotctrl.shell import ShellInteraction
 from riotctrl_shell.netif import Ifconfig
 from riotctrl_shell.gnrc import GNRCPktbufStats
+from riotctrl_shell.loramac import Loramac, LoramacHelpParser
+from riotctrl_shell.sys import Reboot
 from testutils.shell import GNRCLoRaWANSend, ifconfig, lorawan_netif
 from testutils.shell import check_pktbuf
 
 
 GNRC_LORAWAN_APP = "examples/gnrc_lorawan"
+LORAWAN_APP = "examples/lorawan"
+SEMTECH_LORAMAC_APP = "tests/pkg_semtech-loramac"
 pytestmark = pytest.mark.rc_only()
 
 APP_PAYLOAD = "This is RIOT!"
@@ -19,8 +24,16 @@ TTN_UPLINK_DELAY = 5
 TTN_PORT = 1
 OTAA_JOIN_DELAY = 20
 
+# Theoretical duty cycling timeoff for EU863-870
+# https://www.semtech.com/uploads/documents/LoraDesignGuide_STD.pdf#page=7
+TEST_DATA_RATES = {"5": 6.2, "3": 20.6, "0": 164.6}
+
 
 class ShellGnrcLoRaWAN(Ifconfig, GNRCLoRaWANSend, GNRCPktbufStats):
+    pass
+
+
+class ShellLoramac(Reboot, Loramac):
     pass
 
 
@@ -59,6 +72,126 @@ def run_lw_test(node, ttn_client, iface, dev_id):
     assert ttn_client.downlink_ack_received()
 
     check_pktbuf(node)
+
+
+@pytest.mark.iotlab_creds
+# nodes passed to riot_ctrl fixture
+@pytest.mark.parametrize(
+    "nodes", [pytest.param(["b-l072z-lrwan1"])], indirect=["nodes"]
+)
+def test_task01(riot_ctrl, ttn_client):
+    node = riot_ctrl(0, LORAWAN_APP, ShellInteraction)
+
+    for _ in range(0, 5):
+        node.riotctrl.term.expect_exact("Sending: This is RIOT!")
+        time.sleep(LORAWAN_APP_PERIOD_S)
+        assert ttn_client.pop_uplink_payload() == APP_PAYLOAD
+
+
+@pytest.mark.iotlab_creds
+# nodes passed to riot_ctrl fixture
+@pytest.mark.parametrize(
+    "nodes", [pytest.param(["b-l072z-lrwan1"])], indirect=["nodes"]
+)
+def test_task02(riot_ctrl, ttn_client, deveui, appeui, appkey):
+    node = riot_ctrl(0, SEMTECH_LORAMAC_APP, ShellLoramac)
+    keys = {
+        "appeui": appeui,
+        "deveui": deveui,
+        "appkey": appkey,
+    }
+
+    for key, time_off in TEST_DATA_RATES.items():
+        # Set keys
+        for k, v in keys.items():
+            node.loramac_set(k, v)
+        node.loramac_set("dr", key)
+        node.loramac_join("otaa")
+        node.loramac_tx(APP_PAYLOAD, cnf=True, port=123)
+        time.sleep(time_off)
+        assert ttn_client.pop_uplink_payload() == APP_PAYLOAD
+        node.loramac_tx(APP_PAYLOAD, cnf=False, port=42)
+        time.sleep(TTN_UPLINK_DELAY)
+        assert ttn_client.pop_uplink_payload() == APP_PAYLOAD
+        node.reboot()
+
+
+@pytest.mark.iotlab_creds
+# nodes passed to riot_ctrl fixture
+@pytest.mark.parametrize(
+    "nodes", [pytest.param(["b-l072z-lrwan1"])], indirect=["nodes"]
+)
+def test_task03(riot_ctrl, ttn_client, devaddr, nwkskey, appskey):
+    node = riot_ctrl(0, SEMTECH_LORAMAC_APP, ShellLoramac)
+    keys = {
+        "appskey": appskey,
+        "nwkskey": nwkskey,
+        "devaddr": devaddr,
+        "rx2_dr": RX2_DR,
+    }
+
+    # Make sure the eeprom is flushed
+    node.loramac_eeprom_erase()
+    node.reboot()
+
+    for key, time_off in TEST_DATA_RATES.items():
+        # Set keys
+        for k, v in keys.items():
+            node.loramac_set(k, v)
+        node.loramac_set("dr", key)
+        # Reset ul_cnt to for NS mac reset: TTN will negotiate RX
+        # configurations that are currently not backed up in the eeprom
+        node.loramac_set("ul_cnt", "0")
+        node.loramac_join("abp")
+        node.loramac_tx(APP_PAYLOAD, cnf=True, port=123, timeout=10)
+        time.sleep(time_off)
+        payload = ttn_client.pop_uplink_payload()
+        assert payload == APP_PAYLOAD
+        node.loramac_tx(APP_PAYLOAD, cnf=False, port=42, timeout=10)
+        time.sleep(TTN_UPLINK_DELAY)
+        payload = ttn_client.pop_uplink_payload()
+        assert payload == APP_PAYLOAD
+        node.reboot()
+
+
+@pytest.mark.iotlab_creds
+# nodes passed to riot_ctrl fixture
+@pytest.mark.parametrize(
+    "nodes", [pytest.param(["b-l072z-lrwan1"])], indirect=["nodes"]
+)
+# pylint: disable=R0913
+def test_task04(riot_ctrl, appeui, deveui, appkey, devaddr, nwkskey, appskey):
+    node = riot_ctrl(0, SEMTECH_LORAMAC_APP, ShellLoramac)
+    parser = LoramacHelpParser()
+
+    if not parser.has_eeprom(node.loramac_help()):
+        return
+
+    # Reset values
+    node.loramac_eeprom_erase()
+    node.reboot()
+
+    # Keys to test
+    keys = {
+        "appeui": appeui,
+        "deveui": deveui,
+        "appkey": appkey,
+        "appskey": appskey,
+        "nwkskey": nwkskey,
+        "devaddr": devaddr,
+    }
+
+    # Set keys
+    for k, v in keys.items():
+        node.loramac_set(k, v)
+
+    # Save and reboot
+    node.loramac_eeprom_save()
+    node.reboot()
+
+    # Check saved keys
+    for k, v in keys.items():
+        assert v in node.loramac_get(k)
 
 
 @pytest.mark.iotlab_creds
